83be92ee3021707bec64dde8c09753dc5157a257
Alejandro Guzman <ketokunxd@gmail.com>
2023-04-03 18:09:17 -0500

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Alejandro Guzman Bolivar on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
